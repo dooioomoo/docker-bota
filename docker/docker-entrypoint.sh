168d5449659b8a45526d711e5e6eae6fecc43375
ryan <dooioomoo@gmail.com>
2020-08-22 07:33:08 +0900

    更新腳本格式

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-SETSOURCE=$1;
-BOTAUSERNAME=$2;
-BOTAPASSWORD=$3;
-DNS1=8.8.8.8;
-DNS2=8.8.4.4;
+SETSOURCE=$1
+BOTAUSERNAME=$2
+BOTAPASSWORD=$3
+DNS1=8.8.8.8
+DNS2=8.8.4.4
 
-BTFILE='/etc/init.d/bt';
+BTFILE='/etc/init.d/bt'
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-echo nameserver $DNS1 > /etc/resolv.conf
-echo nameserver $DNS2 >> /etc/resolv.conf
-echo options ndots:0 >> /etc/resolv.conf
+echo nameserver $DNS1 >/etc/resolv.conf
+echo nameserver $DNS2 >>/etc/resolv.conf
+echo options ndots:0 >>/etc/resolv.conf
 
-if [ $1 != '/usr/sbin/init' ];then
+if [ $1 != '/usr/sbin/init' ]; then
 
-	
 	if [ ! -f "$BTFILE" ]; then
 		set -eux
 		# install baota
 		wget -O install.sh $SETSOURCE
 		echo y | sh install.sh
 		sed -i 's/set_panel_username()/set_panel_username(sys.argv[2])/' /www/server/panel/tools.py
-	  	# 清除登陆限制
+		# 清除登陆限制
 		rm -f /www/server/panel/data/admin_path.pl
 		# 修改面板username
 		cd /www/server/panel && python tools.py username $BOTAUSERNAME
@@ -33,14 +32,12 @@ if [ $1 != '/usr/sbin/init' ];then
 
 	else
 
-	  echo "BOTA已安装！"
-	  
+		echo "BOTA已安装！"
 
 	fi
 
 	exit
-	
-fi
 
+fi
 
 exec "$@"
