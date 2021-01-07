521ed424ae3a6a7f63cb1ab081cb4f4eaaf6e29f
dooioomoo <dooioomoo@gmail.com>
2021-01-07 15:26:49 +0900

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -15,6 +15,22 @@ echo nameserver $DNS1 >/etc/resolv.conf
 echo nameserver $DNS2 >>/etc/resolv.conf
 echo options ndots:0 >>/etc/resolv.conf
 
+
+# WSLIP=127.0.0.1
+# addText="WSLIP"
+# #判断 file.sh 文件中是否存在该字符串
+# if ! grep -q -e "$addText" /etc/hosts
+# then
+# #不存在，添加字符串
+#    echo "input file"
+#    echo "$(sed '$a127.0.0.1'" $addText"'' /etc/hosts)" > /etc/hosts
+# else
+# #存在，不做处理
+#    echo "not input file"
+#    echo "$(sed 's/.*'"$addText"'.*/192.168.1.1 '"$addText"'/' /etc/hosts)" > /etc/hosts
+
+# fi
+
 if [ $1 != '/usr/sbin/init' ]; then
 
 	if [ ! -f "$BTFILE" ]; then
@@ -31,9 +47,11 @@ if [ $1 != '/usr/sbin/init' ]; then
 		cd /www/server/panel && python tools.py panel $BOTAPASSWORD
 
 	else
-
+		# 修改面板username
+		cd /www/server/panel && python tools.py username $BOTAUSERNAME
+		# 修改面板密码
+		cd /www/server/panel && python tools.py panel $BOTAPASSWORD
 		echo "BOTA已安装！"
-
 	fi
 
 	exit
